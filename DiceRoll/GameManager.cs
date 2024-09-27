cc7720e006aa14dad65aad2b652b74feabb1150e
M Hops <70164577+m-hops@users.noreply.github.com>
2024-09-26 19:49:58 -0700

    Final fix

--- DiceRoll/GameManager.cs
+++ DiceRoll/GameManager.cs
@@ -14,7 +14,10 @@ namespace Lab2
         public string playerInput = "";
         public string weaponUsed = "";
 
-        public int rollDelay = 3000;
+        public int pScore;
+        public int cScore;
+
+        public int rollDelay = 6000;
 
         public void GameLoop()
         {
@@ -49,7 +52,7 @@ namespace Lab2
 
         public void Combat()
         {
-            Console.WriteLine("While out on a scouting mission, " + playerName + " recieves a transmission from nearby ship.");
+            Console.WriteLine("While out on a scouting mission, " + playerName + " recieves a transmission from a nearby ship.");
             Console.WriteLine("");
             Console.WriteLine("'Attention captain of the " + shipName + "! Prepared to be fired upon because I think you suck.'");
             Console.WriteLine("");
@@ -62,71 +65,71 @@ namespace Lab2
             if (playerInput == "d6" || playerInput == "6" || playerInput == "laser")
             {
                 weaponUsed = "laser";
-                DieRoller pScore = new DieRoller();
-                DieRoller cScore = new DieRoller();
+                DieRoller pRoll = new DieRoller();
+                DieRoller cRoll = new DieRoller();
 
-                pScore.Roll(6);
+                pScore = pRoll.Roll(6);
                 Task.Delay(rollDelay);
-                cScore.Roll(6);
+                cScore = cRoll.Roll(6);
 
                 Console.WriteLine("");
-                Console.WriteLine("You fire your ships" + weaponUsed + " for " + pScore + "damage");
-                Console.WriteLine("The enemy ship also fires its" + weaponUsed + " at you for " + cScore + "damage");
+                Console.WriteLine("You fire your ships " + weaponUsed + " for " + pScore + " damage");
+                Console.WriteLine("The enemy ship also fires its " + weaponUsed + " at you for " + cScore + " damage");
                 Console.WriteLine("");
 
             } else if (playerInput == "d10" || playerInput == "10" || playerInput == "missle")
             {
                 weaponUsed = "missle";
-                DieRoller pScore = new DieRoller();
-                DieRoller cScore = new DieRoller();
+                DieRoller pRoll = new DieRoller();
+                DieRoller cRoll = new DieRoller();
 
-                pScore.Roll(10);
+                pScore = pRoll.Roll(10);
                 Task.Delay(rollDelay);
-                cScore.Roll(10);
+                cScore = cRoll.Roll(10);
 
                 Console.WriteLine("");
-                Console.WriteLine("You fire your ships" + weaponUsed + " for " + pScore + "damage");
-                Console.WriteLine("The enemy ship also fires its" + weaponUsed + " at you for " + cScore + "damage");
+                Console.WriteLine("You fire your ships " + weaponUsed + " for " + pScore + " damage");
+                Console.WriteLine("The enemy ship also fires its " + weaponUsed + " at you for " + cScore + " damage");
                 Console.WriteLine("");
 
             } else if (playerInput == "d12" || playerInput == "12" || playerInput == "torpedo")
             {
                 weaponUsed = "torpedo";
-                DieRoller pScore = new DieRoller();
-                DieRoller cScore = new DieRoller();
+                DieRoller pRoll = new DieRoller();
+                DieRoller cRoll = new DieRoller();
 
-                pScore.Roll(12);
+                pScore = pRoll.Roll(12);
                 Task.Delay(rollDelay);
-                cScore.Roll(12);
+                cScore = cRoll.Roll(12);
 
                 Console.WriteLine("");
-                Console.WriteLine("You fire your ships" + weaponUsed + " for " + pScore + "damage");
-                Console.WriteLine("The enemy ship also fires its" + weaponUsed + " at you for " + cScore + "damage");
+                Console.WriteLine("You fire your ships " + weaponUsed + " for " + pScore + " damage.");
+                Console.WriteLine("The enemy ship also fires its " + weaponUsed + " at you for " + cScore + " damage.");
                 Console.WriteLine("");
 
             } else if (playerInput == "d20" || playerInput == "20" || playerInput == "nuke")
             {
                 weaponUsed = "nuke";
-                DieRoller pScore = new DieRoller();
-                DieRoller cScore = new DieRoller();
+                DieRoller pRoll = new DieRoller();
+                DieRoller cRoll = new DieRoller();
 
-                pScore.Roll(20);
+                pScore = pRoll.Roll(20);
                 Task.Delay(rollDelay);
-                cScore.Roll(20);
+                cScore = cRoll.Roll(20);
 
                 Console.WriteLine("");
-                Console.WriteLine("You fire your ships" + weaponUsed + " for " + pScore + "damage");
-                Console.WriteLine("The enemy ship also fires its" + weaponUsed + " at you for " + cScore + "damage");
+                Console.WriteLine("You fire your ships " + weaponUsed + " for " + pScore + " damage.");
+                Console.WriteLine("The enemy ship also fires its " + weaponUsed + " at you for " + cScore + " damage.");
                 Console.WriteLine("");
 
             } else
             {
-                DieRoller pScore = new DieRoller();
-                DieRoller cScore = new DieRoller();
+                DieRoller pRoll = new DieRoller();
+                DieRoller cRoll = new DieRoller();
 
-                pScore.Roll(0);
+                pScore = pRoll.Roll(0);
                 Task.Delay(rollDelay);
-                cScore.Roll(20);
+                cScore = cRoll.Roll(20);
 
                 Console.WriteLine("");
                 Console.WriteLine("The " + shipName + "s computer informs you your weapons systems have locked up.");
@@ -159,8 +162,32 @@ namespace Lab2
                 Console.WriteLine("It's a tie!");
             }
 
-            Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Would you like to play again?[y/n]");
+            Console.WriteLine("");
+
+            playerInput = Console.ReadLine();
+
+            if (playerInput == "y" || playerInput == "yes") {
+
+                Console.Clear();
+                DefaultVal();
+                GameLoop();
+
+            }
+            else {
+            }
         }
         
+        public void DefaultVal()
+        {
+            playerName = "";
+            shipName = "";
+            playerInput = "";
+            weaponUsed = "";
+
+            pScore = 0;
+            cScore = 0;
+    }
     }
 }
